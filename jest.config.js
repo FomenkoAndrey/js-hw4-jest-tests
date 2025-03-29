19c54b37becc367530cb94e7ca6ced280ddd9ae8
Alecto <fomenkoandrey1978@gmail.com>
2025-03-29 08:34:55 +0200

    Refactor(tests): migrate from Jest to Vitest and move tsts to __tests__ foldere

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  coverageProvider: 'v8',
-  testEnvironment: 'jsdom', // або 'jsdom' для веб-проектів
-  testMatch: ['**/*.test.js'] // патерн для знаходження тестових файлів
-}
-
-module.exports = config
--- src/__tests__/nestedFunctions.test.js
+++ src/__tests__/nestedFunctions.test.js
@@ -1,4 +1,4 @@
-import { outerFunction } from './nestedFunctions'
+import { outerFunction } from '../nestedFunctions'
 
 test('multiplies 2 * 3 * 4 to equal 24', () => {
   expect(outerFunction(2)(3)(4)).toBe(24)
@@ -10,4 +10,4 @@ test('multiplies -5 * 2 * 10 to equal -100', () => {
 
 test('multiplies 1 * 5 * -3 to equal -15', () => {
   expect(outerFunction(1)(5)(-3)).toBe(-15)
-})
+}) 
